d9d36fff709d43a6b93171e6b0ea3c4f3db5e217
SzymonRogozinski <pokeszymon.pl@gmail.com>
2024-09-30 21:34:39 +0200

    fix border render

--- Code/src/GUI/WalkingGUI/WalkingPanel.java
+++ Code/src/GUI/WalkingGUI/WalkingPanel.java
@@ -9,18 +9,20 @@ import java.awt.*;
 
 //Panel for rendering map view
 public class WalkingPanel extends JPanel {
+
+    private final int margin=3;
     private final int scale;
     private final int viewSize;
 
     public WalkingPanel() {
         //set panel
-        this.setSize(GUISettings.PANEL_SIZE,GUISettings.PANEL_SIZE);
+        this.setSize(GUISettings.PANEL_SIZE-margin*2,GUISettings.PANEL_SIZE-margin*2);
         this.setLayout(null);
         //this.setBackground(Color.BLACK);
         this.setOpaque(true);
 
         viewSize= FogOfWar.getSize();
-        scale= GUISettings.PANEL_SIZE / FogOfWar.getSize();
+        scale= (GUISettings.PANEL_SIZE-margin*2) / FogOfWar.getSize();
     }
 
     @Override
@@ -29,7 +31,7 @@ public class WalkingPanel extends JPanel {
 
         for (int i = 0; i < viewSize; i++) {
             for (int j = 0; j < viewSize; j++) {
-                g2D.drawImage(GameManager.getWalkingManager().getWalking().getMap().getPlace(j+ GameManager.getWalkingManager().getWalking().fogOfWar.getMinX(), i+ GameManager.getWalkingManager().getWalking().fogOfWar.getMinY()).getImage(), j * scale, i * scale, scale, scale, null);
+                g2D.drawImage(GameManager.getWalkingManager().getWalking().getMap().getPlace(j+ GameManager.getWalkingManager().getWalking().fogOfWar.getMinX(), i+ GameManager.getWalkingManager().getWalking().fogOfWar.getMinY()).getImage(), j * scale+margin, i * scale+margin, scale, scale, null);
             }
         }
     }
